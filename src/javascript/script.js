6fd4e6474767eafa7a9d7b93e44a3192028dc492
fernandoJesuss <fernando_ffernando@hotmail.com>
2024-09-27 22:10:03 -0300

    feat: Novas funcionalidades

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,20 +1,22 @@
+const list = document.querySelector("ul");
+const buttonShowAll = document.querySelector(".show-all");
+let myLi = "";
 
+function showAll() {
+    menuOptions.forEach((product) => {
+        myLi += `
+    <li>
+    
+    <img src=${Product.src}>
+    <p>${product.name}</p>
+    <p class="item-price">R$ ${product.price}</p>
+    
+    </li>
+    
+    `
+    });
 
+    list.innerHTML = myLi;
+}
 
-const menuOptions = [
-
-    { name: 'X-Salada', price: 30, vegan: false, src: './img/xsalada.jpeg' },
-    
-    { name: 'X-Bacon', price: 34, vegan: false, src: './img/xbacon.png' },
-    
-    { name: 'X-Bacon Egg', price: 39, vegan: false, src: './img/bacon-egg.png' },
-    
-    { name: 'Monstruoso', price: 50, vegan: false, src: './img/monstruoso.png' },
-    
-    { name: 'Big Vegano', price: 55, vegan: true, src: './img/xvegan.png' },
-    
-    { name: 'X-Vegan', price: 45, vegan: true, src: './img/monstruoso-vegan.png' },
-    
-    ]
-    
-    
+buttonShowAll.addEventListener("click", showAll);
