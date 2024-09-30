677090059d6eac4f3d67c6476cb0230c2e151041
fernandoJesuss <fernando_ffernando@hotmail.com>
2024-09-30 12:54:42 -0300

    Feat:novas funcionalidades

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,6 +1,11 @@
+
+// ForEch
+
 const list = document.querySelector("ul");
 const buttonShowAll = document.querySelector(".show-all");
 const buttonmapAll = document.querySelector(".map-all");
+const sumAll = document.querySelector(".sum-all");
+
 let myLi = "";
 
 function showAll(productsArray) {
@@ -18,6 +23,8 @@ function showAll(productsArray) {
     list.innerHTML = myLi;
 }
 
+// Map
+
 function mapAllItems() {
     const newPrices = menuOptions.map((products) => ({
         ...products,
@@ -27,8 +34,28 @@ function mapAllItems() {
 
     showAll(newPrices);
 
-
 }
+
+// Reduce
+
+function sumAllItems() {
+
+    const totalValue = menuOptions.reduce((acc, curr) => acc + curr.price, 0)
+
+    list.innerHTML =  `
+      <li>
+       <p>O Valor total dos itens é R$: ${totalValue}</p>
+       </li>  
+
+         `
+
+};
+
+
+
+
+
 
 buttonShowAll.addEventListener("click", () => showAll(menuOptions));
 buttonmapAll.addEventListener("click", mapAllItems);
+sumAll.addEventListener("click", sumAllItems);
